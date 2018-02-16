fab7c0de13f6bdb32c2516338a7ab532515cc1ae
Hasan U <31290654+Scprt@users.noreply.github.com>
2018-02-16 10:06:13 +0300

    Update PortDetector.py

--- SerPortDetec/PortDetector.py
+++ SerPortDetec/PortDetector.py
@@ -1,7 +1,7 @@
 import serial.tools.list_ports
 flag = False
-#device_ID = 'USB VID:PID=2341:0042 SNR=85430353631351A002F1'
-#device_ID = 'USB VID:PID=10c4:ea60 SNR=0001'
+#device_ID = 'USB VID:PID=2341:0042 SNR=81231233631351A002F1' //  MY Arduino
+#device_ID = 'USB VID:PID=10c4:ea60 SNR=0121'			// RP Lidar
 def Controller(device_ID):
 	try:
 		for p in list(serial.tools.list_ports.comports()):
