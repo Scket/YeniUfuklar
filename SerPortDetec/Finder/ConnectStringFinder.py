dc021833f4e0273208bd4f3dd0cf8cf00ce17945
Hasan U <31290654+Scprt@users.noreply.github.com>
2018-02-16 13:58:36 +0300

    Update ConnectStringFinder.py

--- SerPortDetec/Finder/ConnectStringFinder.py
+++ SerPortDetec/Finder/ConnectStringFinder.py
@@ -2,20 +2,17 @@ import serial.tools.list_ports
 connected_list = []
 empty_list = []
 try:
-	_ = raw_input("Unplug  your device and press Enter")
-	for p in list(serial.tools.list_ports.comports()):
-		if not p[2] == 'n/a' : empty_list.append(p)
-	_ = raw_input("Plug into your device and press Enter")
-
-	for p in list(serial.tools.list_ports.comports()):
-		if not p[2] == 'n/a' : connected_list.append(p)
-	for p in range(len(empty_list)):
-		connected_list.remove(empty_list[p])
+	print("[INFO] Waiting for ports")
+	for port in list(serial.tools.list_ports.comports()):
+		if not port[2] == 'n/a' : empty_list.append(port)
+	_ = raw_input("[Plug into your device and press Enter")
+	for port in list(serial.tools.list_ports.comports()):
+		if not port[2] == 'n/a' : connected_list.append(port)
+	for port in range(len(empty_list)):
+		connected_list.remove(empty_list[port])
 	device = connected_list[0]
 	print "[INFO] Your Device Port : " + device[0]
 	print "[INFO] Port Name : " + device[1]
-	print "[INFO] Device_ID (PID and SNR) : " +device[2]
-	
-except:	
-
+	print "[INFO] Device PID and SNR : " +device[2]
+except:
 	print "[ERROR] Please be sure unplugged your device and try again"	
